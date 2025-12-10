769ea62b53033f4975f94b0dea528c82a6dec5f5
qianbao <qianbao_tu@163.com>
2025-12-10 23:44:50 +0100

    next config changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',  // <--- 核心修改：开启静态导出
+  images: {
+    unoptimized: true, // <--- 核心修改：静态模式下无法使用服务器优化，必须关闭
+  },
 };
 
 export default nextConfig;
